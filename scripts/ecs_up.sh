cf1b8cc59cb113b5867a11db5d23e9d6ef606260
Dan Clark <danclark@redhat.com>
2021-01-07 14:02:44 -0500

    Updates to use RHEL and podman

--- scripts/ecs_up.sh
+++ scripts/ecs_up.sh
@@ -3,14 +3,21 @@
 export CLUSTER_NAME='redhat'
 export CLUSTER_CONFIG='redhat'
 export PROFILE_NAME='redhat'
-export KEYPAIR='danclark-personal'
-# RHEL 7
-#export AMI='ami-0c5e670c9f7ab3c15'
-# RHEL 8
-export AMI='ami-0e58243939f4798b0'
 
-ecs-cli up --keypair "${KEYPAIR}" --capability-iam \
-           --size 2 --instance-type t2.medium \
-           --cluster-config "${CLUSTER_CONFIG}" \
-           --ecs-profile "${PROFILE_NAME}" \
-           --image-id "${AMI}"
+# EC2 Keypair for the ECS instances
+export KEYPAIR=''
+# Add in the AMI IDs which will be used to create instances for ECS
+# RHEL 7
+export AMI=''
+# RHEL 8
+export AMI=''
+
+#ecs-cli up --keypair "${KEYPAIR}" --capability-iam \
+#           --size 2 --instance-type t2.medium \
+#           --cluster-config "${CLUSTER_CONFIG}" \
+#           --ecs-profile "${PROFILE_NAME}" \
+#           --image-id "${AMI}"
+
+# Cluster with no nodes added
+ecs-cli up --capability-iam --cluster-config "${CLUSTER_CONFIG}" --ecs-profile "${PROFILE_NAME}" help
+
